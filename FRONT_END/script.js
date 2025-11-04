2496fb6c33c9c5461658912360dbbe9621bf5a26
surafel <surafelmengist2015@gmail.com>
2025-11-04 19:05:38 +0300

    day three introduction  to html5

--- FRONT_END/script.js
+++ FRONT_END/script.js
@@ -1,1 +1,10 @@
-console.log("hello these is the best course around the world")
+const bodyEl= document.querySelector("body");
+const img=document.querySelector("img");
+img.addEventListener("click", ()=>{
+    img.style.display="none";
+    console.log("surafel image is click here");
+})
+console.log(bodyEl)
+bodyEl.addEventListener("mousemove", ()=>{
+    console.log("mouse move here")
+})
